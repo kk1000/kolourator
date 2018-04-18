89359e4280252de5760d58ad64643a8b8c582fae
Michal Lonski <michal.lonski@nokia.com>
2018-04-18 15:19:40 +0200

    Disabled touchable buttons (to prevent exiting game when used by kid), added icons

--- android/src/pl/lonski/kolourator/AndroidLauncher.java
+++ android/src/pl/lonski/kolourator/AndroidLauncher.java
@@ -5,6 +5,8 @@ import java.util.Locale;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -19,5 +21,17 @@ public class AndroidLauncher extends AndroidApplication {
 		};
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Kolourator(speakerProvider), config);
+	}
+
+	@Override
+	public void onBackPressed() {
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		ActivityManager activityManager = (ActivityManager) getApplicationContext()
+				.getSystemService(Context.ACTIVITY_SERVICE);
+		activityManager.moveTaskToFront(getTaskId(), 0);
 	}
 }
